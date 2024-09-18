a3ab3b7ccb14e007e619316e3575cfb3ea662435
alexs <alxgar2000@gmail.com>
2024-09-18 13:11:31 -0600

    Clonado

--- pulga.js
+++ pulga.js
@@ -1,4 +1,5 @@
 function Integrantes() {
     document.body.style.backgroundColor = "red";
     document.getElementById("output").innerText = "Ana Camila Ojeda López";
+    document.getElementById("output1").innerText = "Ricardo Alejandro Garcia Regalado";
 }
